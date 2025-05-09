bd6290b5f41fa145bbd8cb6e76c244aa5e6d73d6
Alex <alexandriaahansen@gmail.com>
2025-05-09 10:01:15 +0200

    Implemented register and login with jwt

---
+++ Api/Middleware/AuthenticateUser.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Claims;
+
+namespace Api.Middleware;
+
+public class AuthenticateUser
+{
+    private readonly RequestDelegate _next;
+
+    public AuthenticateUser(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        if (!context.User.Identity?.IsAuthenticated ?? true)
+        {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsync("Unauthorised: JWT token is missing or invalid.");
+        }
+        var email = context.User.FindFirstValue(ClaimTypes.Email);
+        if (string.IsNullOrEmpty(email))
+        {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsync("Unauthorised: Email claim not found");
+        }
+        context.Items["Email"] = email;
+        await _next(context);
+    }
+     
+}
