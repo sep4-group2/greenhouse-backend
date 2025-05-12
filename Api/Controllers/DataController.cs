72ce25d2060f4c3ffae5170484403dcb1b36fad3
CodeAFox <illeskrisztinacsilla@gmail.com>
2025-05-12 13:58:43 +0200

    MAL: API Endpoint can return past sensor readings
    /api/Data/{greenhouseId} endpoint can now return a list of sensor readings relating to one greenhouse. No filtering has been implemented yet.

--- Api/Controllers/DataController.cs
+++ Api/Controllers/DataController.cs
@@ -11,9 +11,19 @@ public class DataController: ControllerBase
     private readonly AppDbContext _ctx;
     
     [HttpGet("{greenhouseId}")]
-    public async Task<ICollection<SensorReading>> GetPastData([FromRoute] int greenhouseId)
+    public async Task<IActionResult> GetPastDataAsync([FromRoute] int greenhouseId, [FromQuery] DateTime? beforeDate = null, [FromQuery] DateTime? afterDate = null, [FromQuery] string? sensorType = null)
     {
+        List<SensorReading> sensorReadings = new List<SensorReading>();
+        try
+        {
+            //First, load all sensor readings for one specific greenhouse
+            sensorReadings = _ctx.SensorReadings.Where(reading => reading.GreenhouseId == greenhouseId).ToList();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Something went wrong while getting old data from the database for greenhouse {greenhouseId}: {e.Message}");
+        }
         
-        return null;
+        return Ok(sensorReadings);
     }
 }
