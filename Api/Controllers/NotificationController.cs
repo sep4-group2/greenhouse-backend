629ca98bf4c56bc3c62435cc74089b6b35aae05c
CodeAFox <illeskrisztinacsilla@gmail.com>
2025-05-14 15:34:50 +0200

    MAL: Notifications Endpoint for public VAPID keys created

--- Api/Controllers/NotificationController.cs
+++ Api/Controllers/NotificationController.cs
@@ -1,3 +1,4 @@
+using Api.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
@@ -6,5 +7,18 @@ namespace Api.Controllers;
 [Route("api/[controller]")]
 public class NotificationController: ControllerBase
 {
+    //Two endpoints needed 
+    //One for fetching the public VAPID key
+    //One for saving subscriptions
+
     
+    //Endpoint for clients to fetch the public VAPID key needed to subscribe
+    [HttpGet]
+    public IActionResult GetPublicKey()
+    {
+        //The public key needs to be fetched from wherever it is stored
+        //TO BE IMPLEMENTED
+        var publicKey = new VAPIDKeyDTO();
+        return Ok(publicKey);
+    }
 }
