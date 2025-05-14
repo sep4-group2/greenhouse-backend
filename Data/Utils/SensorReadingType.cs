be2a88a3129081474dbc3136864c1b797869a300
CodeAFox <illeskrisztinacsilla@gmail.com>
2025-05-14 11:20:19 +0200

    MAL: Util SensorReadingType.cs has been implemented
    It seemingly works. Included a trial run of how it functions as well in this commit.

---
+++ Data/Utils/SensorReadingType.cs
@@ -0,0 +1,8 @@
+namespace Data.Database.Utils;
+
+public static class SensorReadingType
+{
+    public const string Temperature = "temperature";
+    public const string AirHumidity = "air humidity";
+    public const string SoilHumidity = "soil humidity";
+}
