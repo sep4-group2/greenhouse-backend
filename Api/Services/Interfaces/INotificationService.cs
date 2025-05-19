538eed72a6eca5953d71be5b10f71dd658d1e133
CodeAFox <illeskrisztinacsilla@gmail.com>
2025-05-19 15:02:07 +0200

    MAL: Notifications Minor fixes after merge

--- Api/Services/Interfaces/INotificationService.cs
+++ Api/Services/Interfaces/INotificationService.cs
@@ -8,4 +8,7 @@ public interface INotificationService
     public Task SaveSubscription(SaveSubscriptionDTO subscription);
     public Task SendNotification(SendNotificationDTO notification);
     public Task SaveNotification(SaveNotificationDTO notification);
+
+    public Task<List<NotificationResultDTO>> GetNotificationsForPeriodAsync(
+        int greenhouseId, DateTime start, DateTime end);
 }
